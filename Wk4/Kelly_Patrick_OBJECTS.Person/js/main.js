59a6701bd07ea106de31409832dfb77b8c6b97f4
Hewey <pdkelly@fullsail.edu>
2015-04-02 20:23:38 -0400

    I don't remember

---
+++ Wk4/Kelly_Patrick_OBJECTS.Person/js/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by the JavaScript Development Team
+ * Class: PWA
+ * Goal: Goal7
+ */
+
+// an array with 5 names
+// create a for loop that creates 3 objects stored in an array and when initiating make sure to choose a random person
+// a function that displays the names on a webpage but does not allow the same name to appear twice
+// create an interval that calls the function above that calls it every 30 seconds
+// run a function that updates each of the people
---
+++ Wk4/Kelly_Patrick_OBJECTS.Person/js/person.js
@@ -0,0 +1,7 @@
+/**
+ * Created by the JavaScript Development Team
+ * Class: PWA
+ * Goal: Goal7
+ */
+// add the object containing the people to the global object 
+// create two variables that hold jobs an actions and are on the object hold the people 
